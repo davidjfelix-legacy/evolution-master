2341fc6e0faf6c16ea846873dc38b6c480d603e2
David J. Felix <DavidJFelix@users.noreply.github.com>
2015-12-07 20:04:04 -0500

    Create bootstrap-osx.sh

---
+++ bootstrap/bootstrap-osx.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+xcode-select --install # what if you can't get this... use https://developer.apple.com/downloads
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" # if you can't get this, manually grab it
+
