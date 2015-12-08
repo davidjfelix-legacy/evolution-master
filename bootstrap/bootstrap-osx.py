52a13a711985b92afc103dad337daef290db699a
David J. Felix <DavidJFelix@users.noreply.github.com>
2015-12-07 21:06:12 -0500

    Update and rename bootstrap-osx.sh to bootstrap-osx.py

---
+++ bootstrap/bootstrap-osx.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# In Yosemite and Mavericks, this is 2.7.10
+from subprocess import Popen
+
+
+def install_xcode_clt_pkg(pkg_file)
+    Popen(['sudo', 'install', '-pkg', pkg_file, '-target', '/']).wait()
+
+
+def mount_xcode_dmg(dmg_file):
+    # FIXME: actually return real mount point... not this ghetto thing
+    Popen(['hdiutil', 'attach', dmg_file]).wait()
+    return "/Volumes/" + dmg_file.split("/")[-1]
+
+
+def install_xcode_cloud():
+    Popen(['xcode-select', '--install']).wait()
+
+
+def install_brew():
+    # FIXME: this won't work by default... need to actually use a shell to get the content
+    Popen(['ruby', '-e', '"$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"']).wait()
+    
