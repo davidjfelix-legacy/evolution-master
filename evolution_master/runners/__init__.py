0c0b205e7f9cfa00a84bc4059c1321cec6c1c63f
David J Felix <felix.davidj@gmail.com>
2016-02-07 22:59:09 +0000

    Remove old python evolution master stuff

--- evolution_master/runners/__init__.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-
-# This file is intentionally blank.
-# It's used by Python as a breadcrumb for determining where the boundaries
-# of a module are, in relation to the file system.
